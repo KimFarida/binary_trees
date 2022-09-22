1864a0313e5e1d0b09ae5c6b1944dec0cc395075
KimFarida <farimomoh@gmail.com>
2022-09-22 03:07:57 -0700

    Task 1 Correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,17 +6,19 @@
  * @value: input value
  * Return: pointer to new node
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
-	if (!parent)
-		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
 	new_node->parent = parent;
+	if(parent == NULL)
+		return (NULL);
 	if (parent->left)
 	{
 		new_node->left = parent->left;
