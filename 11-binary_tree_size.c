38498f43d6b35e889a7b304b9f039791cb0a6f55
KimFarida <farimomoh@gmail.com>
2022-09-22 06:36:32 -0700

    Task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: input binary tree
+ * Return: number of descendant child nodes
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t a, b;
+	a = binary_tree_size(tree->left);
+	b = binary_tree_size(tree->>right);
+
+	if (!tree)
+		return (0);
+
+	return (1 + a + b);
+}
